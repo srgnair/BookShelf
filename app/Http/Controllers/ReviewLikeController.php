2adee068799f11b1a997287f01f07a5777bc9442
airi <air.shirogane@gmail.com>
2026-08-16 06:48:34 +0900

    feat: レビューに対するいいね機能を実装

--- app/Http/Controllers/ReviewLikeController.php
+++ app/Http/Controllers/ReviewLikeController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
 class ReviewLikeController extends Controller
 {
-    //
+    public function toggle(Request $request, Review $review): RedirectResponse
+    {
+        $request->user()
+            ->likedReviews()
+            ->toggle($review->id);
+
+        return back();
+    }
 }
