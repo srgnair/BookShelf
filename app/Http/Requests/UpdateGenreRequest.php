8d18b1647ef5e1007ae328ddb618facda2e30440
airi <air.shirogane@gmail.com>
2026-08-16 07:51:18 +0900

    feat: ジャンルCRUD機能を実装

---
+++ app/Http/Requests/UpdateGenreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateGenreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', Rule::unique('genres')->ignore($this->genre)],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name' => 'ジャンル名',
+        ];
+    }
+}
