3270a4a54f724f75bcbde86ea1316a09d8ec420d
airi <air.shirogane@gmail.com>
2026-08-16 07:06:25 +0900

    feat: ランキング機能を実装

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\View\View;
+
 class RankingController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $rankedBooks = Book::withAvg('reviews', 'rating')
+            ->withCount('reviews')
+            ->has('reviews')
+            ->orderByDesc('reviews_avg_rating')
+            ->orderByDesc('reviews_count')
+            ->orderBy('id')
+            ->take(10)
+            ->get();
+
+        return view('ranking.index', compact('rankedBooks'));
+    }
 }
